975e42e7b363c23fa40f984e83da9012e9cd7422
kipngenokev <kkipngeno409@gmail.com>
2025-09-26 15:47:10 +0300

    created bookController class and added the QueryMapping annotation

---
+++ GraphQL-Application/src/main/java/com/graphQl/GraphQL/Application/BookController.java
@@ -0,0 +1,16 @@
+package com.graphQl.GraphQL.Application;
+
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class BookController {
+
+    @QueryMapping
+    public List<Book> books() {
+        return Book.books;
+    }
+
+}
